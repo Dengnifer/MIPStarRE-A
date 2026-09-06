00f97f6f21df6a138f60c0782db88f38024bd561
Dengnifer <ruixuan.deng@icloud.com>
2026-09-06 14:17:49 +0800

    feat(QPBT/Games): Preserve consistency under tensor convolution

--- MIPStarRE/QPBT.lean
+++ MIPStarRE/QPBT.lean
@@ -22,6 +22,7 @@ import MIPStarRE.QPBT.Games.ErrorFunctions
 import MIPStarRE.QPBT.Games.Consistency
 import MIPStarRE.QPBT.Games.StrategyClasses
 import MIPStarRE.QPBT.Games.DistanceTheorems
+import MIPStarRE.QPBT.Games.DistanceTheorems.TensorConsistency
 import MIPStarRE.QPBT.Games.Sandwich
 import MIPStarRE.QPBT.Games.CondLinearTheorems
 import MIPStarRE.QPBT.Games.TypedCondLinear
